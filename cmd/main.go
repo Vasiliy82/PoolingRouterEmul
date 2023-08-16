995d346fb26e7fb2f4914e219942a1308f9f398b
Василий Селезнев <v.seleznev@automacon.ru>
2023-08-17 01:10:58 +0300

    Первая почти рабочая версия

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/PoolingRouterEmul/internal/model"
 )
@@ -10,32 +11,32 @@ import (
 func main() {
 
 	request_arr := []byte(`{
-		"traffic_jams":"true",
+		"traffic_jams":true,
 		"couriers":[
 		   {
 			  "id":"101",
 			  "travel_modes":"driving",
-			  "speed":"10",
-			  "max_orders":"10",
-			  "max_distance":"1000",
-			  "max_weight":"10",
-			  "queue_number":"1",
-			  "has_terminal":"false",
-			  "pickup_time":"10",
-			  "hand_over_time":"10",
+			  "speed":10,
+			  "max_orders":2,
+			  "max_distance":1000,
+			  "max_weight":10,
+			  "queue_number":1,
+			  "has_terminal":false,
+			  "pickup_time":"00:10:00",
+			  "hand_over_time":"00:05:00",
 			  "delivery_services":["1", "2"]
 		   },
 		   {
 			"id":"102",
 			"travel_modes":"walking",
-			"speed":"5",
-			"max_orders":"10",
-			"max_distance":"1000",
-			"max_weight":"10",
-			"queue_number":"1",
-			"has_terminal":"true",
-			"pickup_time":"10",
-			"hand_over_time":"10",
+			"speed":5,
+			"max_orders":2,
+			"max_distance":1000,
+			"max_weight":10,
+			"queue_number":2,
+			"has_terminal":true,
+			"pickup_time":"00:10:00",
+			"hand_over_time":"00:05:00",
 			"delivery_services":["1", "3"]
 		 }
 
@@ -48,45 +49,137 @@ func main() {
 					"id":"10001",
 					"shop_no":"201",
 					"pickup_point":{
-					   "latitude":"37.5",
-					   "longitude":"55.6",
-					   "main":"true"
+					   "latitude":37.5,
+					   "longitude":55.6,
+					   "main":true
 					}
 				 },
 				 {
 					"id":"10002",
 					"shop_no":"201",
 					"pickup_point":{
-					   "latitude":"37.5",
-					   "longitude":"55.6",
-					   "main":"false"
+					   "latitude":37.5,
+					   "longitude":55.6,
+					   "main":false
 					}
 				 },
 				 {
 					"id":"10003",
 					"shop_no":"201",
 					"pickup_point":{
-					   "latitude":"37.5",
-					   "longitude":"55.6",
-					   "main":"false"
+					   "latitude":37.5,
+					   "longitude":55.6,
+					   "main":false
 					}
 				 }
 
 			  ],
 			  "delivery_point":{
 				 "location":{
-					"latitude":"37.6",
-					"longitude":"55.5"
+					"latitude":37.6,
+					"longitude":55.5
 				 }
 			  },
-			  "weight":"3",
+			  "weight":3,
 			  "delivery_services":["1"],
-			  "needs_terminal":"false",
+			  "needs_terminal":false,
 			  "date_supply":"2023-08-10 07:55:12",
 			  "date_supply_until_soft":"2023-08-10 08:55:12",
 			  "date_supply_until_hard":"2023-08-10 09:55:12",
 			  "service_id":"1"
-		   }
+		   },
+		   {
+			"id":"10004",
+			"orders":[
+			   {
+				  "id":"10004",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":true
+				  }
+			   },
+			   {
+				  "id":"10005",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":false
+				  }
+			   },
+			   {
+				  "id":"10006",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":false
+				  }
+			   }
+
+			],
+			"delivery_point":{
+			   "location":{
+				  "latitude":37.6,
+				  "longitude":55.5
+			   }
+			},
+			"weight":3,
+			"delivery_services":["1"],
+			"needs_terminal":false,
+			"date_supply":"2023-08-10 07:55:12",
+			"date_supply_until_soft":"2023-08-10 08:55:12",
+			"date_supply_until_hard":"2023-08-10 09:55:12",
+			"service_id":"1"
+		 },
+		 {
+			"id":"10007",
+			"orders":[
+			   {
+				  "id":"10007",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":true
+				  }
+			   },
+			   {
+				  "id":"10008",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":false
+				  }
+			   },
+			   {
+				  "id":"10009",
+				  "shop_no":"201",
+				  "pickup_point":{
+					 "latitude":37.5,
+					 "longitude":55.6,
+					 "main":false
+				  }
+			   }
+
+			],
+			"delivery_point":{
+			   "location":{
+				  "latitude":37.6,
+				  "longitude":55.5
+			   }
+			},
+			"weight":3,
+			"delivery_services":["1"],
+			"needs_terminal":false,
+			"date_supply":"2023-08-10 07:55:12",
+			"date_supply_until_soft":"2023-08-10 08:55:12",
+			"date_supply_until_hard":"2023-08-10 09:55:12",
+			"service_id":"1"
+		 }
 		]
 	 }`)
 
@@ -99,6 +192,91 @@ func main() {
 		return
 	}
 
+	results := model.Results{
+		Status:  "Ok",
+		Message: "Ok",
+	}
+
+	droppedLocations := map[string]any{}
+	for _, p := range request.Parcels {
+		droppedLocations[p.ID] = p
+	}
+
 	fmt.Printf("%#v", request)
 
+	for _, c := range request.Couriers {
+		orders := 0
+		nodesPickup := []model.ResultRouteNode{}
+		nodesDelivery := []model.ResultRouteNode{}
+
+		for _, p := range request.Parcels {
+			// проверяем, не отдали ли кому-то этот парцел
+			_, present := droppedLocations[p.ID]
+			if present {
+				// если курьеру можно взять еще один парцел
+				if orders < c.MaxOrders {
+					// начинаем докидывать заказы в массивы "взять-отдать"
+					for _, o := range p.Orders {
+						nodePickup := model.ResultRouteNode{
+							Action:    "pickup",
+							PointType: "pickup",
+							ParcelID:  p.ID,
+							OrderID:   o.ID,
+							Location: struct {
+								Latitude  float64 "json:\"latitude\""
+								Longitude float64 "json:\"longitude\""
+							}{
+								Latitude:  o.PickupPoint.Latitude,
+								Longitude: o.PickupPoint.Longitude,
+							},
+							DistanceFromPrevious: 1,
+							DurationFromPrevious: "00:01:00",
+						}
+						nodeDelivery := model.ResultRouteNode{
+							Action:    "delivery",
+							PointType: "delivery",
+							ParcelID:  p.ID,
+							OrderID:   o.ID,
+							Location: struct {
+								Latitude  float64 "json:\"latitude\""
+								Longitude float64 "json:\"longitude\""
+							}{
+								Latitude:  p.DeliveryPoint.Location.Latitude,
+								Longitude: p.DeliveryPoint.Location.Longitude,
+							},
+							DistanceFromPrevious: 1,
+							DurationFromPrevious: "00:01:00",
+						}
+						nodesPickup = append(nodesPickup, nodePickup)
+						nodesDelivery = append(nodesDelivery, nodeDelivery)
+
+					}
+					// считаем, что парцел взяли, т.е. убираем его из droppedLocations[p.ID]
+					delete(droppedLocations, p.ID)
+					orders++
+				}
+			}
+		}
+		// Если по итогу цикла по парцелам курьер схватил хоть что-то, надо добавить элемент в список Routes
+		if orders > 0 {
+			// соединяем массивы воедино и чиним нумерацию
+			nodes := append(nodesPickup, nodesDelivery...)
+			for i := range nodes {
+				nodes[i].NodeID = 1 + i
+			}
+
+			elem := model.ResultRoute{
+				CourierID:             c.ID,
+				DateShipTarget:        time.Now().Add(10 * time.Minute).Format(time.RFC3339),
+				DateShipTargetExtreme: time.Now().Add(15 * time.Minute).Format(time.RFC3339),
+				EstimatedTime:         time.Now().Add(120 * time.Minute).Format(time.RFC3339),
+				TotalDistance:         1,
+				TotalDuration:         1,
+				Nodes:                 nodes,
+			}
+			results.Data.Routes = append(results.Data.Routes, elem)
+		}
+	}
+	fmt.Println("Ready")
+	fmt.Printf("%#v\n", results)
 }
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,16 +1,16 @@
 package model
 
 type Request struct {
-	TrafficJams string `json:"traffic_jams"`
+	TrafficJams bool `json:"traffic_jams"`
 	Couriers    []struct {
 		ID               string   `json:"id"`
 		TravelModes      string   `json:"travel_modes"`
-		Speed            string   `json:"speed"`
-		MaxOrders        string   `json:"max_orders"`
-		MaxDistance      string   `json:"max_distance"`
-		MaxWeight        string   `json:"max_weight"`
-		QueueNumber      string   `json:"queue_number"`
-		HasTerminal      string   `json:"has_terminal"`
+		Speed            float64  `json:"speed"`
+		MaxOrders        int      `json:"max_orders"`
+		MaxDistance      float64  `json:"max_distance"`
+		MaxWeight        float64  `json:"max_weight"`
+		QueueNumber      int      `json:"queue_number"`
+		HasTerminal      bool     `json:"has_terminal"`
 		PickupTime       string   `json:"pickup_time"`
 		HandOverTime     string   `json:"hand_over_time"`
 		DeliveryServices []string `json:"delivery_services"`
@@ -21,20 +21,20 @@ type Request struct {
 			ID          string `json:"id"`
 			ShopNo      string `json:"shop_no"`
 			PickupPoint struct {
-				Latitude  string `json:"latitude"`
-				Longitude string `json:"longitude"`
-				Main      string `json:"main"`
+				Latitude  float64 `json:"latitude"`
+				Longitude float64 `json:"longitude"`
+				Main      bool    `json:"main"`
 			} `json:"pickup_point"`
 		} `json:"orders"`
 		DeliveryPoint struct {
 			Location struct {
-				Latitude  string `json:"latitude"`
-				Longitude string `json:"longitude"`
+				Latitude  float64 `json:"latitude"`
+				Longitude float64 `json:"longitude"`
 			} `json:"location"`
 		} `json:"delivery_point"`
-		Weight              string   `json:"weight"`
+		Weight              float64  `json:"weight"`
 		DeliveryServices    []string `json:"delivery_services"`
-		NeedsTerminal       string   `json:"needs_terminal"`
+		NeedsTerminal       bool     `json:"needs_terminal"`
 		DateSupply          string   `json:"date_supply"`
 		DateSupplyUntilSoft string   `json:"date_supply_until_soft"`
 		DateSupplyUntilHard string   `json:"date_supply_until_hard"`
@@ -46,6 +46,31 @@ type Response struct {
 	IDRequest string `json:"id_request"`
 }
 
+type ResultRouteNode struct {
+	NodeID    int    `json:"node_id"`
+	Action    string `json:"action"`
+	ParcelID  string `json:"parcel_id"`
+	OrderID   string `json:"order_id"`
+	PointType string `json:"point_type"`
+	Location  struct {
+		Latitude  float64 `json:"latitude"`
+		Longitude float64 `json:"longitude"`
+	} `json:"location"`
+	DistanceFromPrevious float64 `json:"distance_from_previous"`
+	DurationFromPrevious string  `json:"duration_from_previous"`
+}
+
+type ResultRoute struct {
+	CourierID             string            `json:"courier_id"`
+	DateShipTarget        string            `json:"date_ship_target"`
+	DateShipTargetExtreme string            `json:"date_ship_target_extreme"`
+	CalculationTime       string            `json:"calculation_time"`
+	TotalDistance         float64           `json:"total_distance"`
+	TotalDuration         float64           `json:"total_duration"`
+	EstimatedTime         string            `json:"estimated_time"`
+	Nodes                 []ResultRouteNode `json:"nodes"`
+}
+
 type Results struct {
 	IDRequest string `json:"id_request"`
 	Status    string `json:"status"`
@@ -55,27 +80,6 @@ type Results struct {
 		DroppedLocations []struct {
 			ParcelID string `json:"parcel_id"`
 		} `json:"dropped_locations"`
-		Routes []struct {
-			CourierID             string `json:"courier_id"`
-			DateShipTarget        string `json:"date_ship_target"`
-			DateShipTargetExtreme string `json:"date_ship_target_extreme"`
-			CalculationTime       string `json:"calculation_time"`
-			TotalDistance         string `json:"total_distance"`
-			TotalDuration         string `json:"total_duration"`
-			EstimatedTime         string `json:"estimated_time"`
-			Nodes                 []struct {
-				NodeID    string `json:"node_id"`
-				Action    string `json:"action"`
-				ParcelID  string `json:"parcel_id"`
-				OrderID   string `json:"order_id"`
-				PointType string `json:"point_type"`
-				Location  struct {
-					Latitude  string `json:"latitude"`
-					Longitude string `json:"longitude"`
-				} `json:"location"`
-				DistanceFromPrevious string `json:"distance_from_previous"`
-				DurationFromPrevious string `json:"duration_from_previous"`
-			} `json:"nodes"`
-		} `json:"routes"`
+		Routes []ResultRoute `json:"routes"`
 	} `json:"data"`
 }
